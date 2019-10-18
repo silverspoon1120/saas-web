fbbfd30a12b86b914f04917fcd2b5198edee33c1
Silver Spoon <silverspoon20230101@gmail.com>
2019-10-18 12:21:04 -0700

    typescript-eslint instead of tslint (no support)

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { ServerStyleSheets } from '@material-ui/styles';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
 import { GA_TRACKING_ID } from '../lib/consts';
@@ -20,7 +20,11 @@ class MyDocument extends Document {
     return {
       ...initialProps,
       // Styles fragment is rendered after the app and page rendering finish.
-      styles: <React.Fragment>{sheets.getStyleElement()}</React.Fragment>,
+      styles: (
+        <React.Fragment>
+          {sheets.getStyleElement()}
+        </React.Fragment>
+      ),
     };
   };
 
@@ -30,7 +34,7 @@ class MyDocument extends Document {
       this.props.__NEXT_DATA__.props.initialState.user.darkTheme;
 
     return (
-      <Html lang="en">
+      <html lang="en">
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -135,7 +139,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </Html>
+      </html>
     );
   }
 
