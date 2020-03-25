fee49ca92e4c2507deb975e2850436412f6e0bc1
Silver Spoon <silverspoon20230101@gmail.com>
2020-03-24 18:17:58 -0700

    book/5-end missing types

--- api/server/app.ts
+++ api/server/app.ts
@@ -11,8 +11,7 @@ import * as mongoose from 'mongoose';
 import * as path from 'path';
 
 import api from './api';
-import { setupGoogle } from './google-auth';
-import { setupPasswordless } from './passwordless-auth';
+import { setupGoogle, setupPasswordless } from './auth';
 import { setup as realtime } from './realtime';
 import { stripeWebHooks } from './stripe';
 
